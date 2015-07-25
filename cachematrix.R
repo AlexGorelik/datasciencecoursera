8de74c977cf674cf5b9cd43855025a24e5600b31
Alex Gorelik <alex.v.gorelik@gmail.com>
2015-07-25 16:48:10 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -11,7 +11,7 @@ makeCacheMatrix <- function(x = matrix(), tol = sqrt(.Machine$double.eps)) {
     m <<- NULL
   }
   get <- function() x
-  ## set_inv_mat <- function (solve) m <<- solve
+  ## WENT WITH ginv() AS I WAS NOT SURE I WAS IMPLEMENTING solve() ARGUMENTS RIGHT set_inv_mat <- function (solve) m <<- solve
   set_inv_mat <- function (ginv) m <<- ginv
   get_inv_mat <- function () m
   list(set = set, get = get, set_inv_mat = set_inv_mat, get_inv_mat = get_inv_mat)
@@ -21,17 +21,17 @@ makeCacheMatrix <- function(x = matrix(), tol = sqrt(.Machine$double.eps)) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x = matrix(), ...) {
-        ## Return a matrix that is the inverse of 'x'
-        ## temp <- as.data.frame(get_inv_mat)
+        NEVERMIND: Return a matrix that is the inverse of 'x'
+        ## NEVERMIND: temp <- as.data.frame(get_inv_mat)
         m <- x$get_inv_mat()
-        ## m <- x$temp()
+        ## NEVERMIND:  m <- x$temp()
         if(!is.null(m)) {
           message ("Getting cashed data for matrix...")
           return (m)
         }
         else {
         mtrx <- x$get()
-        ## m <- solve(mtrx) %*% mtrx
+        ## WENT WITH ginv() INSTEAD m <- solve(mtrx) %*% mtrx
         m <- ginv(mtrx) %*% mtrx
         x$set_inv_mat(mtrx)
         m
